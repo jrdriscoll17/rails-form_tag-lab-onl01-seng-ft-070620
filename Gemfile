c37a6907e5c07545e64478d2e3dd8912186d4a5c
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-09-05 01:51:17 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
-source 'https://rubygems.org'
+# frozen_string_literal: true
 
+source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails'
@@ -23,12 +24,11 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-
 group :development, :test do
   gem 'byebug'
-  gem 'spring'
-  gem 'rspec-rails'
-  gem 'factory_bot_rails'
   gem 'capybara'
   gem 'database_cleaner'
+  gem 'factory_bot_rails'
+  gem 'rspec-rails'
+  gem 'spring'
 end
--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'Route to view' do
@@ -9,8 +11,8 @@ end
 
 describe 'Multiple students' do
   it 'shows them on the index page' do
-    Student.create!(first_name: "Daenerys", last_name: "Targaryen")
-    Student.create!(first_name: "Lindsey", last_name: "Stirling")
+    Student.create!(first_name: 'Daenerys', last_name: 'Targaryen')
+    Student.create!(first_name: 'Lindsey', last_name: 'Stirling')
 
     visit students_path
     expect(page).to have_content(/Daenerys|Lindsey/)
@@ -20,24 +22,24 @@ end
 describe 'form page' do
   it 'renders the form with the new action' do
     visit new_student_path
-    expect(page).to have_content("Student Form")
+    expect(page).to have_content('Student Form')
   end
 
   it 'ensures that the new form submits content and renders form content' do
     visit new_student_path
 
-    fill_in 'student[first_name]', with: "Margaery"
-    fill_in 'student[last_name]', with: "Tyrell"
+    fill_in 'student[first_name]', with: 'Margaery'
+    fill_in 'student[last_name]', with: 'Tyrell'
 
-    click_on "Submit Student"
+    click_on 'Submit Student'
 
-    expect(page).to have_content("Margaery")
+    expect(page).to have_content('Margaery')
   end
 end
 
 describe 'Show page' do
   before do
-    @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
+    @student = Student.create!(first_name: 'Daenerys', last_name: 'Targaryen')
   end
 
   it 'renders properly' do
@@ -47,18 +49,18 @@ describe 'Show page' do
 
   it 'renders the first name in a h1 tag' do
     visit student_path(@student)
-    expect(page).to have_css("h1", text: "Daenerys")
+    expect(page).to have_css('h1', text: 'Daenerys')
   end
 
   it 'renders the last name in a h1 tag' do
     visit student_path(@student)
-    expect(page).to have_css("h1", text: "Targaryen")
+    expect(page).to have_css('h1', text: 'Targaryen')
   end
 end
 
 describe 'linking from the index page to the show page' do
   it 'index page links to post page' do
-    @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
+    @student = Student.create!(first_name: 'Daenerys', last_name: 'Targaryen')
     visit students_path
     expect(page).to have_link(@student.to_s, href: student_path(@student))
   end
